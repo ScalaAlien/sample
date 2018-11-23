a2e2b6277c0ee1a1908da6eaeaf9b6a67a484e42
ScalaAlien <scala.alien@gmail.com>
2018-11-23 19:30:26 +0900

    Add JavaScript Hello Spec

---
+++ test/controllers/HomeControllerSpec.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import org.scalatest._
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.test.Helpers._
+import play.api.test._
+
+class HomeControllerSpec extends FunSpec
+  with MustMatchers
+  with GuiceOneAppPerSuite {
+
+  describe("HomeController") {
+    describe("route of HomeController#hello") {
+      it("should be valid") {
+        val result = route(app, FakeRequest(GET, routes.HomeController.hello.toString)).get
+        status(result) mustBe OK
+      }
+    }
+  }
+}
